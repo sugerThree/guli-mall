baae81dfe2ce62cdfd1af80082117c534b50fd98
three <501989996@qq.com>
2022-07-29 20:53:28 +0800

    提交本地库

---
+++ gulimall-product/src/test/java/com/three/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.gulimall.product;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
